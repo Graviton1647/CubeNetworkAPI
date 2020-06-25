2661ae12e601c490b6c5b4c9dfc8211cb41b7c9b
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-06-25 22:41:00 +0100

    Revert

--- src/main/kotlin/org/cube/api/CubePlugin.kt
+++ src/main/kotlin/org/cube/api/CubePlugin.kt
@@ -1,19 +1,64 @@
 package org.cube.api
 
+import mu.KotlinLogging
+import org.bukkit.Bukkit
 import org.bukkit.plugin.java.JavaPlugin
+import org.cube.api.commands.CommandManager
+import org.cube.api.events.EventLoader
+import kotlin.system.measureTimeMillis
 
 
-class CubePlugin : JavaPlugin() {
+abstract class CubePlugin : JavaPlugin() {
 
-    lateinit var manager : BasicPlugin
+    companion object {
+
+        lateinit var plugin : JavaPlugin
+
+        val logger = KotlinLogging.logger {  }
+
+    }
 
     override fun onEnable() {
-        manager = BasicPlugin(this)
+        plugin = this
+        makeData()
+        logger.info { "Starting ${this.name}" }
+        val time =  measureTimeMillis {
+            EventLoader.load(this)
+            val commands = CommandManager(this)
+            commands.loadCommands(this)
+            commands.loadTabComplete()
+            start()
+        }
+        logger.info { "${this.name} Started up in [${time.toDouble() / 1000.0}] seconds." }
     }
 
     override fun onDisable() {
-
+        stop()
+        Bukkit.getOnlinePlayers().forEach {
+            it.closeInventory()
+        }
+        logger.info { "Stopped ${this.name}" }
     }
 
+    /**
+     * Make the data folder if it does not exists
+     */
+    private fun makeData() {
+        if(!dataFolder.exists()) {
+            dataFolder.mkdirs()
+        }
+    }
+
+
+
+    /**
+     * Called when the plugin is enabled
+     */
+    abstract fun start()
+
+    /**
+     * Called when the plugin is disabled
+     */
+    abstract fun stop()
 
 }
