e4edbd1b0b20d30d4f554e6b2e575544d2c0e2f6
Graviton <44570024+Graviton1647@users.noreply.github.com>
2020-06-25 21:50:53 +0100

    5.6

--- src/main/kotlin/org/cube/api/CubePlugin.kt
+++ src/main/kotlin/org/cube/api/CubePlugin.kt
@@ -5,7 +5,11 @@ import org.bukkit.plugin.java.JavaPlugin
 
 abstract class CubePlugin : JavaPlugin() {
 
-   lateinit var manager : BasicPlugin
+    companion object {
+
+        lateinit var manager : BasicPlugin
+
+    }
 
     override fun onEnable() {
         manager = BasicPlugin(this)
